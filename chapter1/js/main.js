acab462b48c70e6e0f6d126e433bd5d323882907
Rocky Vuong <rocky.vuong0@gmail.com>
2014-08-28 18:38:05 +1000

    js file refractored and added fadeIn effect

--- chapter1/js/main.js
+++ chapter1/js/main.js
@@ -1,9 +1,28 @@
 var main = function(){
 	"use strict";
 	
-	$(".comment-input button").on("click", function(event){
-		console.log("Hello world");
+	var $new_comment;
+	
+	//Add comment from hitting enter key
+	$(".comment-input input").on("keypress", function(event){
+		if(event.keyCode === 13){
+			addComment();
+		}
 	});
+
+	//Add comment from clicking on + button
+	$(".comment-input button").on("click", function(event){
+		addComment();
+	});
+	
+	//Function to add comment
+	var addComment = function(){
+		$new_comment = $("<p>").text($(".comment-input input").val());
+		$new_comment.hide();
+		$(".comments").append($new_comment);
+		$new_comment.fadeIn();
+		$(".comment-input input").val("");
+	};
 };
 
 $(document).ready(main);
